6ef01654928d40d0a44960e3837e36500366ddbc
Marcin <marcin.boguniecki@gmail.com>
2020-04-10 16:15:38 +0200

    added basic functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // application monster_generator
+
+const monster = ["vampire","werewolf","ghoul","zombie","skeleton","ork"];
+
+const getRandomMonster = () => monster[Math.floor(Math.random() * monster.length)];
+
+const setRandomMonster = () => document.getElementById('random_monster').innerText = getRandomMonster();
+
+
+
+document.getElementById('generate').addEventListener('click', setRandomMonster)
